77ce5e4a30b98a0389ad92a51133784402e874b2
Jjass45678 <jjassalflores8@gmail.com>
2026-01-02 00:33:20 +0800

    conf.js

--- conf.js
+++ conf.js
@@ -1,1 +1,7 @@
+serverBaseUrl= http://brixmodz2.x10.mx/connect
 
+gameActivity=com.tencent.tmgp.cod.CODMainActivity
+
+appTitle=Brix Library System
+
+game1=BRIXMODZ|libbrix23so| REMEMBER: If the menu doesn't appear, please close the mod immediately.
